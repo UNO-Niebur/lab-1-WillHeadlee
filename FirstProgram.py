f798b7638a2a4f0ee38049af2bce42966de5b6cf
WillHeadlee <william.headlee182@gmail.com>
2026-01-22 20:51:40 +0000

    Completed Lab 1 Programs

--- FirstProgram.py
+++ FirstProgram.py
@@ -1,21 +1,21 @@
 #FirstProgram.py
-#Name:
-#Date:
-#Assignment:
+#Name: William Headlee
+#Date: 1/22/2026
+#Assignment: Lab 1
 
 def main():
   print("First Program")
   #Say hello
-  
+  print("Hello")
   #Ask for the user's name
-
+  username = input("What is your name? ")
   #Use the user's name in the program.
-
+  print("Nice to meet you, " + username)
   #Ask the user for their age.
-
+  age = input("Please enter your age: ")
   #Tell the user what year they were born in.
   #Assume that they have not had their birthday yet this year.
-
+  print("You were most likely born in " + str(2025 - int(age)))
 
 #Call the main function if this is the file being run.
 if __name__ == '__main__':
--- MadLib.py
+++ MadLib.py
@@ -1,15 +1,24 @@
 #MadLib.py
-#Name:
-#Date:
-#Assignment:
+#Name: William Headlee
+#Date: 1/22/2026
+#Assignment: Lab 1
 
 def main():
   print("Madlib")
   #Ask user for words
-
+  word1 = input("Give me an adjective: ")
+  word2 = input("Give me an animal: ")
+  word3 = input("Give me a verb ending in -ing: ")
+  word4 = input("Give me a noun: ")
+  word5 = input("Give me something you shout: ")
+  word6 = input("Give me an adverb (ending in -ly): ")
   #Print the story with the user supplied words.
-
-
+  # Use an f-string (notice the 'f' before the quotes)
+  print("\nYesterday, I decided to cook a " + word1 + " dinner for my family. \n" \
+        "Everything was going well until a giant " + word2 + " burst into the kitchen! \n" \
+          "It started " + word3 + " all over the counters. In the chaos, I accidentally \n" \
+            "dropped a/an " + word4 + " into the soup. \"" + word5 + "\" ! I shouted. \n" \
+              "My brother laughed " + word6 + ", and we ended up ordering pizza instead.\n")
 
 #Call the main function if this is the file being run.
 if __name__ == '__main__':
